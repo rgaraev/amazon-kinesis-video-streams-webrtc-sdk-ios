2d1298c768e2604689be41c9589a3ad53613cedc
rgaraev <garaev.ramil@gmail.com>
2020-07-30 18:44:45 +0200

    added accont tabbar

---
+++ Swift/KVSiOSApp/WorkoutTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  WorkoutTableViewCell.swift
+//  AWSKinesisVideoWebRTCDemoApp
+//
+//  Created by user176323 on 7/30/20.
+//
+
+import Foundation
+import UIKit
+
+class WorkoutTableViewCell: UITableViewCell {
+    
+}
+ 
--- Swift/KVSiOSApp/WorkoutsListViewController.swift
+++ Swift/KVSiOSApp/WorkoutsListViewController.swift
@@ -8,14 +8,6 @@
 import Foundation
 import UIKit
 
-class WorkoutsListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
-
+class WorkoutsListViewController: UITableViewController {
+    
 }
